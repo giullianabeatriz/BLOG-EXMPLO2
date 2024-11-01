865fbe932e5e0576f48f1b15edf8ac12fccfc9ff
Giulliana Beatriz <giullianabeatriz13@gmail.com>
2024-11-01 10:24:03 -0300

    Formulário

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Blog, Mensagem
 
 def index(request):
     context ={
-        "posts": Post.objects.all()
+        "posts": Post.objects.all(),
+        "blog": Blog.objects.first()
     }
     return render(request, "index.html", context)
 
@@ -14,7 +15,27 @@ def post(request, post_id):
     return render(request, "post.html", context)
 
 def sobre(request):
-    return render(request, "about.html")
+    context = {
+        "blog": Blog.objects.first()
+    }
+    return render(request, "about.html", context)
 
 def contato(request):
-    return render(request, "contact.html")
+    context = {
+        "blog": Blog.objects.first()
+    }
+    if request.method == "POST":
+        print(request.POST['nome'])
+        print(request.POST['email'])
+        print(request.POST['telefone'])
+        print(request.POST['mensagem'])
+        
+        mensagem = Mensagem(nome = request.POST['nome'],
+                            email = request.POST['email'],
+                            telefone = request.POST['telefone'],
+                            mensagem = request.POST['mensagem'])
+        mensagem.save()
+                 
+        return render(request, "contact.html", context)
+    else:
+        return render(request, "contact.html", context)
