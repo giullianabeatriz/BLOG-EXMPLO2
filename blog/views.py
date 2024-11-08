a7fe9b6d512b2fd4a7bdedbc2fc57f33b4d3160f
Giulliana Beatriz <giullianabeatriz13@gmail.com>
2024-11-08 10:23:58 -0300

    Commit

--- blog/views.py
+++ blog/views.py
@@ -25,17 +25,32 @@ def contato(request):
         "blog": Blog.objects.first()
     }
     if request.method == "POST":
-        print(request.POST['nome'])
-        print(request.POST['email'])
-        print(request.POST['telefone'])
-        print(request.POST['mensagem'])
+        context['erro'] = {}
+        if not request.POST['nome']:
+            context['erro']['nome'] = True
+        if not request.POST['email']:
+            context['erro']['email'] = True
+        if not request.POST['telefone']:
+            context['erro']['telefone'] = True
+        if not request.POST['mensagem']:
+            context['erro']['mensagem'] = True
+        if context['erro']:
+            return render(request, "contact.html", context)
         
         mensagem = Mensagem(nome = request.POST['nome'],
                             email = request.POST['email'],
                             telefone = request.POST['telefone'],
+                            cidade = request.POST['cidade'],
                             mensagem = request.POST['mensagem'])
         mensagem.save()
                  
         return render(request, "contact.html", context)
     else:
         return render(request, "contact.html", context)
+    
+def mensagem(request):
+    context = {
+        "mensagem": Mensagem.objects.all(),
+        "blog": Blog.objects.first()
+    }
+    return render(request, "mensagem.html", context)
